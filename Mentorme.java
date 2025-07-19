0452aa9baa716010d9c6d095914ba17bf663e2ec
ChiragHira <chiraghira39@gmail.com>
2025-07-20 01:16:15 +0530

    m2 method is added

--- Mentorme.java
+++ Mentorme.java
@@ -3,6 +3,10 @@ public class Mentorme {
     public void m1() {
         System.out.println("Method m1 in Mentorme class");
     }
+
+    public void m2() {
+        System.out.println("Method m2 in Mentorme class");
+    }
     public static void main(String[] args) {
         System.out.println("MentorMe Application Started");
         
