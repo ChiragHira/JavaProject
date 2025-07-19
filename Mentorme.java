506b0a329a36e3c81f5877875989bf26d763a44c
ChiragHira <chiraghira39@gmail.com>
2025-07-20 01:37:14 +0530

    exp1 method is added

--- Mentorme.java
+++ Mentorme.java
@@ -7,6 +7,10 @@ public class Mentorme {
     public void m2() {
         System.out.println("Method m2 in Mentorme class");
     }
+
+    public void exp1() {
+        System.out.println("Method exp1 in Mentorme class");
+    }
     public static void main(String[] args) {
         System.out.println("MentorMe Application Started");
         
